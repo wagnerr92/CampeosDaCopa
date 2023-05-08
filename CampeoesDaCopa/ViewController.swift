f46636dcd3ad59e6a0f3e79d301fd38ca9468a18
Wagner Rodrigues <wagnersr92@gmail.com>
2023-05-07 22:55:13 -0300

    Initial Commit

---
+++ CampeoesDaCopa/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CampeoesDaCopa
+//
+//  Created by Wagner Rodrigues on 07/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
